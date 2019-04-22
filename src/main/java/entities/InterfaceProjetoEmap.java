440aa3b029cd800a196052bc32ae4479308d3999
Aluno <Aluno@DESKTOP-3FIS8IH>
2019-04-22 08:13:48 -0300

    22/04/2019 ANA2

--- src/main/java/entities/InterfaceProjetoEmap.java
+++ src/main/java/entities/InterfaceProjetoEmap.java
@@ -1,5 +1,25 @@
 package entities;
 
+import java.util.List;
+
 public interface InterfaceProjetoEmap {
+	
+	public CotaAreaVolume cotaareaVolume (
+			Reservatorio reservatorio, List<Usuario> usuarios)
+					throws ReservatorioExistenteException, UruarioException;
+	
+	public void calculoAreaVolume (Usuario usuario, Reservatorio reservatorio);
+
+	
+}
+	
+	
+					
+			
+			
+			
+			
+			
+	
 
 }
