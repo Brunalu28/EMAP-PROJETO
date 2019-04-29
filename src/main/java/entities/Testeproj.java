29c1fb11b23b12550556c67bf58428b1f3eddb23
Anap123 <anapalbinosouza@gmail.com>
2019-04-29 08:44:21 -0300

    29/04/2019 commit 2

--- src/main/java/entities/Testeproj.java
+++ src/main/java/entities/Testeproj.java
@@ -9,18 +9,19 @@ import java.util.ArrayList;
 import org.junit.Test;
 
 public class Testeproj {
+	
 	@Test
 	void CotaAreaVolume() {
-		ArrayList<Reservatorio> reservatorio = new ArrayList<Reservatorio>();
 		
-		Reservatorio r = r.CotaAreaVolume("Calculo CotaAreaVolume/ testesdaw2", reservatorio);
+		ArrayList<Reservatorio> reservatorio = new ArrayList<Reservatorio>();
+		InterfaceProjetoEmap pe = null;
+		Reservatorio r = pe.cotaareavolume("Calculo CotaAreaVolume/ testesdaw2", reservatorio);
 		
 		assertNotNull (r);
 		
 		assertEquals("Teste Daw",r.getNome());
 		
 	}
-	
 
 }
 
